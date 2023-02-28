aba49915282546e9ed189e011e751d52e653e9d8
NPSoftware68 <thanhtra.sqcb@icloud.com>
2023-02-28 11:28:11 +0700

    Initial Commit

---
+++ FoodPin/Model/Restaurant.swift
@@ -0,0 +1,50 @@
+//
+//  Restaurant.swift
+//  FoodPin
+//
+//  Created by Simon Ng on 16/10/2022.
+//
+
+import Foundation
+
+struct Restaurant {
+    var name: String
+    var type: String
+    var location: String
+    var phone: String
+    var description: String
+    var image: String
+    var isFavorite: Bool
+    
+    enum Rating: String, CaseIterable {
+        case awesome
+        case good
+        case okay
+        case bad
+        case terrible
+
+        var image: String {
+            switch self {
+            case .awesome: return "love"
+            case .good: return "cool"
+            case .okay: return "happy"
+            case .bad: return "sad"
+            case .terrible: return "angry"
+            }
+        }
+    }
+    
+    init(name: String, type: String, location: String, phone: String, description: String, image: String, isFavorite: Bool = false) {
+        self.name = name
+        self.type = type
+        self.location = location
+        self.phone = phone
+        self.description = description
+        self.image = image
+        self.isFavorite = isFavorite
+    }
+    
+    init() {
+        self.init(name: "", type: "", location: "", phone: "", description: "", image: "", isFavorite: false)
+    }
+}
